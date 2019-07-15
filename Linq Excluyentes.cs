5884335e0fa3c3ee2298dac6c906b6e37043ce49
Daniel Delgado <danieldelgadodeveloper@gmail.com>
2019-07-15 14:56:37 -0500

    Update Linq Excluyentes.cs

--- Linq Excluyentes.cs
+++ Linq Excluyentes.cs
@@ -4,8 +4,18 @@
 //teoria de conjuntos (A - B)
 //LEFT JOIN
 var diferentes = (from r in lista1 
-                                   join p in Lista2 on r.Identificador equals p.Identificador
+                                   join p in Lista2 
+                                   on r.Identificador equals p.Identificador
                                    into temporal
                                    from d in temporal.DefaultIfEmpty()
                                    where d == null
                                    select r).ToList();
+
+//Ejemplo 2
+var parametAccion = (from p in this.ItemsParametrizados
+                                join it in this.ProtocoloGuiaSelecta.ItemsProtocoloGuia
+                                on p.Identificador equals it.Modelo.Identificador
+                                into temporal
+                                from t in temporal.DefaultIfEmpty()
+                                where p.TipoItemLinea == ItemsAsociadosProtocolo.Acciones && t == null
+                                select p).ToList();
